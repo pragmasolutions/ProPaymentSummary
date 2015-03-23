25f20328c767b2298b6d65a191ee06ad8840043a
federicofrank <defe.frank@gmail.com>
2015-03-23 19:17:06 -0300

    Initial Commit

---
+++ src/ProPaymentSummary/ProPaymentSummary.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,38 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace ProPaymentSummary.Web
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery")
+                .Include("~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval")
+                .Include("~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryplugins")
+                .Include("~/Scripts/jquery-plugins/jquery.isotope.min.js")
+                .Include("~/Scripts/jquery-plugins/jquery.prettyPhoto.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr")
+                .Include("~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap")
+                .Include("~/Scripts/bootstrap.js",
+                         "~/Scripts/respond.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/app")
+                .Include("~/Scripts/main.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css")
+                .Include("~/Content/css/bootstrap.min.css")
+                .Include("~/Content/css/font-awesome.min.css")
+                .Include("~/Content/css/prettyPhoto.css")
+                .Include("~/Content/css/Site.css")
+                .Include("~/Content/css/main.css"));
+        }
+    }
+}
---
+++ src/ProPaymentSummary/ProPaymentSummary.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProPaymentSummary.Web.MvcApplication" Language="C#" %>
