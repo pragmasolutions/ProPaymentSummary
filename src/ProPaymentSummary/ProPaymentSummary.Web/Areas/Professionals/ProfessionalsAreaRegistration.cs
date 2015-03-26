1d79edef2bffd5e54f9a0f1640b3f3dd9c38ecb5
federicofrank <defe.frank@gmail.com>
2015-03-25 21:02:59 -0300

    Agregado area para profecionales

---
+++ src/ProPaymentSummary/ProPaymentSummary.Web/Areas/Professionals/ProfessionalsAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace ProPaymentSummary.Web.Areas.Professionals
+{
+    public class ProfessionalsAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Professionals";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Professionals_default",
+                "Professionals/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
