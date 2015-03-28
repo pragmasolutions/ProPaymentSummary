4e464627ddf8932f9c2b6c14d171c7b0d484d34d
jrossiq <jrossiq@gmail.com>
2015-03-28 20:26:15 -0300

    separated controller for user profile

---
+++ src/ProPaymentSummary/ProPaymentSummary.Web/Areas/Professionals/app/js/controllers/user/profileController.js
@@ -0,0 +1,19 @@
+﻿(function () {
+
+    'use strict';
+
+    var module = angular.module('xenon.controllers');
+
+    module.controller('UserProfileCtrl', function ($scope, $element) {
+        $scope.LastName = "Rossi Querin";
+        $scope.FirstName = "Juan Pablo";
+        $scope.Address = "Brasil 1060";
+        $scope.ProvinceId = "1";
+        $scope.City = "Corrientes";
+        $scope.Phone = "4433687";
+        $scope.CUIT = "20-32405064-8";
+        $scope.DNI = "32405064";
+        $scope.MN = "123456";
+        $scope.ProfileImageUrl = '/Areas/Professionals/assets/images/user-4.png';
+    });
+})()
